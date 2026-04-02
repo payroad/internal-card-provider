2ed06d9e6ae3df97619f68057d4f512615e887e6
Nick <nickolay.zykov@silverbird.com>
2026-04-02 21:38:55 +0700

    update namespace: Domain/PaymentFlow → Domain/Channel

--- src/Data/InternalCardAttemptData.php
+++ src/Data/InternalCardAttemptData.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Payroad\Provider\InternalCard\Data;
 
-use Payroad\Domain\PaymentFlow\Card\CardAttemptData;
-use Payroad\Domain\PaymentFlow\Card\ThreeDSData;
+use Payroad\Domain\Channel\Card\CardAttemptData;
+use Payroad\Domain\Channel\Card\ThreeDSData;
 
 final class InternalCardAttemptData implements CardAttemptData
 {
